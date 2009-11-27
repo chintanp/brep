da83e7df67e100a7151ea5db021e2ceca951a762
daniel <daniel@frink>
2009-11-27 14:40:13 -0300

    Fazendo o Corner.

--- src/interface/GLCanvas.h
+++ src/interface/GLCanvas.h
@@ -25,6 +25,9 @@ public:
     void clear();
 
     void addCube(float minX, float minY, float minZ, float size);
+    void addCorner(float minX, float minY, float minZ,
+                    float midX, float midY, float midZ,
+                    float maxX, float maxY, float maxZ);
 
 private:
     void onPaint(wxPaintEvent& event);

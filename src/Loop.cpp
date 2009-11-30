29dbccd0df49b9389d968dc17ffcd5aea94e0112
caio <caio@caio>
2009-11-30 07:28:38 -0300

    Atribuindo normais para as faces. Ainda est? um pouco estranho, acredito que se deve ao fato das faces serem desenhadas seguindo a orienta??o CW.

--- src/Loop.cpp
+++ src/Loop.cpp
@@ -7,3 +7,28 @@ Loop::Loop(Face *f) {
     face->addLoop(this);
 }
 
+void Loop::setNormal() {
+    HalfEdge *h = this->hed;
+    HalfEdge *h2 = h->mate();
+    Vec3 v(h2->origin->x - h->origin->x,
+           h2->origin->y - h->origin->y,
+           h2->origin->z - h->origin->z);
+
+    while((h = h->next) != this->hed) {
+        h2 = h->mate();
+        Vec3 u(h2->origin->x - h->origin->x,
+               h2->origin->y - h->origin->y,
+               h2->origin->z - h->origin->z);
+        //Verifica se dois vetores são colineares
+        //v.u = |v||u|cos(theta)
+        //Se os vetores forem colineares, cos(theta) == 1,
+        //logo v.u == |v||u|
+        if(dot(u, v) == u.length()*v.length())
+            continue;
+        else {
+            this->normal = normalize(cross(v, u));
+            break;
+        }
+    }
+}
+
--- src/Loop.h
+++ src/Loop.h
@@ -2,12 +2,17 @@
 #define _LOOP_H_
 
 #include "HalfEdge.h"
+#include "math/Vec3.h"
 
 class Face;
 
 class Loop {
 public:
     Loop(Face *f);
+    
+    void setNormal();
+
+    Vec3 normal;
     
     //void setFace(Face *f);
     HalfEdge *hed;

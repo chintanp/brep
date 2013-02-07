230e1ec1688bed8de5de1e480851ba921acb8142
caio <caio@localhost>
2013-02-06 22:17:17 -0300

    Adicionando o trab de novo, agora em qt.

---
+++ src/interface/GLWidget.h
@@ -0,0 +1,106 @@
+#ifndef GLWIDGET_H
+#define GLWIDGET_H
+
+#include <QtOpenGL>
+#include "Scene.h"
+#include <set>
+
+
+inline float degrees(float radians)
+{ return 180.0 * radians * M_1_PI; }
+
+inline float radians(float degrees)
+{ return degrees * M_PI/180.0; }
+
+enum Mode {EDIT, DRAW};
+class GLWidget : public QGLWidget
+{
+    Q_OBJECT
+
+public:
+    GLWidget(QWidget *parent = 0);
+    ~GLWidget();
+
+    void switchMode();
+    void setMeshSelect();
+    void setFaceSelect();
+    void setEdgeSelect();
+    void setVertexSelect();
+    void setStartLineLoop() {startLineLoop=!startLineLoop;}
+
+    void doneDrawing();
+
+    void sweep(float dX, float dY, float dZ);
+
+    Scene scene;
+    Scene drawScene;
+
+public slots:
+    void showGrid(bool);
+    void setPerspective(bool);
+
+protected:
+    void initializeGL();
+	void paintGL();
+    void resizeGL(int width, int height);
+
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent * event);
+    void wheelEvent(QWheelEvent* event);
+    void keyPressEvent(QKeyEvent *);
+
+protected Q_SLOTS:
+    void deleteMesh();
+    void move();
+    void neighborhood();
+    void rotate();
+    void scale();
+    void translate();
+
+private:
+	void drawGrid();
+	void drawAxis(GLfloat *mat);
+	void drawBackground();
+    void drawEditBackground();
+    void shotPicking(int x, int y);
+	void processHits(GLint hits, GLuint buffer[]);
+	void loadProjection();
+
+	bool show_grid;
+	bool perspective;
+	bool selectionMode;
+
+
+    //Camera
+    float zoom_fat;
+    float pivot[3];
+    float zoomDepth;
+    float curquat[4];
+    float lastquat[4];
+    bool spinning, moving;
+    int beginx, beginy;
+	float clipClose;
+    float fovy;
+    float panFactor;
+
+    enum
+    {
+		SPIN_BUTTON = Qt::LeftButton,
+		PICK_BUTTON = Qt::RightButton
+	};
+
+    Mode currentMode;
+    bool selectMesh, selectFace, selectEdge, selectVertex;
+    bool startLineLoop;
+    int numPts;
+
+    std::set<Mesh*> meshList;
+    std::set<Loop*> faceList;
+    std::set<Edge*> edgeList;
+    std::set<Vertex*> vertexList;
+
+    void draw(int x, int y);
+};
+
+#endif
